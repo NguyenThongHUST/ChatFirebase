48f11b5f817b8683e968516da3e0656afc02be5a
hieunv183534 <hieu.nv183534@sis.hust.edu.vn>
2022-01-08 16:35:53 +0700

    init

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,55 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-app.js";
+import { getFirestore, collection, doc, setDoc, addDoc, query, where, getDocs, orderBy } from 'https://www.gstatic.com/firebasejs/9.4.0/firebase-firestore.js';
+
 const chatForm = document.getElementById('chat-form');
 const chatMessages = document.querySelector('.chat-messages');
 const roomName = document.getElementById('room-name');
 const userList = document.getElementById('users');
+var db = null;
+
+var app = null;
+const firebaseConfig = {
+  apiKey: "AIzaSyAgfZ8M7rPNPBnqzK6gYlnIiMgeuXEVqW4",
+  authDomain: "thong-pro.firebaseapp.com",
+  projectId: "thong-pro",
+  storageBucket: "thong-pro.appspot.com",
+  messagingSenderId: "729502608583",
+  appId: "1:729502608583:web:74b5917d91887495b2a2e2"
+}
+
+window.onload = async () => {
+  app = initializeApp(firebaseConfig);
+  db = getFirestore(app);
+
+  // try {
+  //   await addDoc(collection(db, "chat"), {
+  //     username: "Nguyễn Văn Hiếu",
+  //     msg: 'Xin chào!',
+  //     room: "JavaScript"
+  //   });
+  //   await addDoc(collection(db, "chat"), {
+  //     username: "Nguyễn Trọng Thông",
+  //     msg: 'Xin chào1!',
+  //     room: "JavaScript"
+  //   });
+  //   await addDoc(collection(db, "chat"), {
+  //     username: "Obama",
+  //     msg: 'Xin chào2!',
+  //     room: "Python"
+  //   });
+  //   console.log("Document written with ID: ", docRef.id);
+  // } catch (e) {
+  //   console.error("Error adding document: ", e);
+  // }
+
+  const q = query(collection(db, "chat"), where("room", "==", room), orderBy('createdAt'));
+  const querySnapshot = await getDocs(q);
+  querySnapshot.forEach((doc) => {
+    console.log(doc.id, " => ", JSON.stringify(doc.data()));
+    outputMessage1(doc.data());
+  });
+
+}
 
 // Get username and room from URL
 const { username, room } = Qs.parse(location.search, {
@@ -29,7 +77,7 @@ socket.on('message', (message) => {
 });
 
 // Message submit
-chatForm.addEventListener('submit', (e) => {
+chatForm.addEventListener('submit', async (e) => {
   e.preventDefault();
 
   // Get message text
@@ -43,11 +91,59 @@ chatForm.addEventListener('submit', (e) => {
 
   // Emit message to server
   socket.emit('chatMessage', msg);
+  var data = { username, msg, room, createdAt: new Date() }
+
+  try {
+    await addDoc(collection(db, "chat"), data);
+
+
+
+  } catch (error) {
+    console.log(error);
+  }
 
   // Clear input
   e.target.elements.msg.value = '';
   e.target.elements.msg.focus();
 });
+
+function outputMessage1(message) {
+  const div = document.createElement('div');
+  div.classList.add('message');
+  const p = document.createElement('p');
+  p.classList.add('meta');
+  p.innerText = message.username;
+  p.innerHTML += `<span>${ formatDateTime(new Date(message.createdAt*1000))}</span>`;
+  
+  div.appendChild(p);
+  const para = document.createElement('p');
+  para.classList.add('text');
+  para.innerText = message.msg;
+  if(message.username == username){
+    p.classList.add('to-right');
+    para.classList.add('to-right');
+  }
+  div.appendChild(para);
+  document.querySelector('.chat-messages').appendChild(div);
+}
+
+function formatDateTime(_date) {
+  if (_date != null) {
+    var date = new Date(_date);
+    var day = date.getDate();
+    day = (day < 10) ? '0' + day : day;
+    var month = date.getMonth() + 1;
+    month = (month < 10) ? '0' + month : month;
+    var hour = date.getHours();
+    hour = (hour < 10) ? '0' + hour : hour;
+    var minit = date.getMinutes();
+    minit = (minit < 10) ? '0' + minit : minit;
+    return hour + ":" + minit + " " + day + '/' + month;
+  }
+  else {
+    return '';
+  }
+}
 
 // Output message to DOM
 function outputMessage(message) {
@@ -64,6 +160,7 @@ function outputMessage(message) {
   div.appendChild(para);
   document.querySelector('.chat-messages').appendChild(div);
 }
+
 
 // Add room name to DOM
 function outputRoomName(room) {
